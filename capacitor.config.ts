5fc9d6fd07f044cf92a18b48afd9bb5810187c85
Eastly1997 <jw.bodoso@gmail.com>
2025-06-10 23:03:14 +0800

    [ARCHITECTURE] update capacitor directory

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'brgy-service',
-  webDir: 'www'
+  webDir: 'public',
 };
 
 export default config;
