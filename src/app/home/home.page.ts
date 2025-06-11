8142b60fe05a572ef70dbd257c0113132358cd31
Eastly1997 <jw.bodoso@gmail.com>
2025-06-11 18:42:17 +0800

    [ARCHITECTURE] Fix Lint problems

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,36 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent,
-   IonCard, IonCardTitle, IonCardHeader, IonCardSubtitle, IonCardContent,
-   IonGrid, IonCol, IonRow,
-  } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardTitle,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardContent,
+  IonGrid,
+  IonCol,
+  IonRow,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   imports: [
-    IonHeader, IonToolbar, IonTitle, IonContent,
-    IonCard, IonCardTitle, IonCardHeader, IonCardSubtitle, IonCardContent, 
-    IonGrid, IonCol, IonRow,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardTitle,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardContent,
+    IonGrid,
+    IonCol,
+    IonRow,
   ],
 })
 export class HomePage {
