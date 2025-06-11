12d9b929c9deafc4c85843a34166987aea4dabab
Eastly1997 <jw.bodoso@gmail.com>
2025-06-12 00:56:14 +0800

    [WorkFlow] Fix lint issue

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core'
-import { provideRouter } from '@angular/router'
-import { routes } from './app.routes'
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
-import { AngularFireModule } from '@angular/fire/compat'
-import { AngularFireAuthModule } from '@angular/fire/compat/auth'
-import { environment } from '../environments/environment'
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from '../app/app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,7 +14,7 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom([
       AngularFireModule.initializeApp(environment.firebaseConfig),
       AngularFireAuthModule,
-      AngularFirestoreModule
-    ])
-  ]
-}
+      AngularFirestoreModule,
+    ]),
+  ],
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
-import { Routes } from '@angular/router'
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: async () => await import('./home/home.page').then((m) => m.HomePage)
+    loadComponent: async () =>
+      await import('../app/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
-  }
-]
+    pathMatch: 'full',
+  },
+];
