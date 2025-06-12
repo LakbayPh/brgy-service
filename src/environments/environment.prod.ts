4b5ba1d147fc1fbf195fba38292e71b045331230
Eastly1997 <jw.bodoso@gmail.com>
2025-06-12 16:26:10 +0800

    [Firebase] update environment for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-}
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCxOknEvFiloeH-TwjVsEeZzPOHDcYDcDM',
+    authDomain: 'barangayeservice.firebaseapp.com',
+    projectId: 'barangayeservice',
+    storageBucket: 'barangayeservice.firebasestorage.app',
+    messagingSenderId: '816829006971',
+    appId: '1:816829006971:web:47645f0138a29c4e708ede',
+    measurementId: 'G-Y713YXKLN4',
+  },
+};
